72c1219c4f674639fd559c730859a6335bc8d13e
meeraj kanaparthi <meeraj.kanaparthi@teradata.com>
2019-05-10 10:03:57 +0530

    basic test

--- src/app/services/calculator.service.spec.ts
+++ src/app/services/calculator.service.spec.ts
@@ -2,13 +2,21 @@ import { TestBed } from '@angular/core/testing';
 
 import { CalculatorService } from './calculator.service';
 
+import {LoggerService} from './logger.service';
+
 describe('CalculatorService', () => {
   it('should add two numbers', () => {
-      pending();
+     // pending();
+    const calculator = new CalculatorService(new LoggerService());
+    const result = calculator.add(2, 2 );
+    expect(result).toBe(4);
   });
 
   it('should subtract two numbers', () => {
-    pending();
+    const calculator = new CalculatorService(new LoggerService());
+    const result = calculator.subtract(2, 2 );
+    expect(result).toBe(0, 'Not expecting exception');
+    // pending();
     // fail();
   });
 
