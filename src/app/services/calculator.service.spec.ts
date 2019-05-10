281f1e8dee118ce9dfed9d3ee46a47c8bad87e95
meeraj kanaparthi <meeraj.kanaparthi@teradata.com>
2019-05-10 10:15:04 +0530

    spies

--- src/app/services/calculator.service.spec.ts
+++ src/app/services/calculator.service.spec.ts
@@ -6,18 +6,20 @@ import {LoggerService} from './logger.service';
 
 describe('CalculatorService', () => {
   it('should add two numbers', () => {
-     // pending();
-    const calculator = new CalculatorService(new LoggerService());
+    // const logger = new LoggerService();
+    const logger = jasmine.createSpyObj('LoggerService', ['log']);
+    logger.log.and.returnValue();
+    const calculator = new CalculatorService(logger);
+    // spyOn(logger, 'log');
     const result = calculator.add(2, 2 );
     expect(result).toBe(4);
+    expect(logger.log).toHaveBeenCalledTimes(1);
   });
 
   it('should subtract two numbers', () => {
     const calculator = new CalculatorService(new LoggerService());
     const result = calculator.subtract(2, 2 );
     expect(result).toBe(0, 'Not expecting exception');
-    // pending();
-    // fail();
   });
 
 });
