cfcc1beea6b5e7ebd92b685a0c4e8a32e20917ff
meeraj kanaparthi <meeraj.kanaparthi@teradata.com>
2019-05-26 20:58:34 +0530

    adjusted test

--- src/app/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses-card-list/courses-card-list.component.spec.ts
@@ -30,11 +30,24 @@ describe('CourseCardListComponent', () => {
   it('should display the course list', () => {
     component.courses = setupCourses();
     fixture.detectChanges();
-    console.log(el.nativeElement.outerHTML);
 
     const cards = el.queryAll(By.css('.course-card'));
     expect(cards).toBeTruthy('Could not finds cards');
     expect(cards.length).toBe(12, 'Unexpected number of courses');
+  });
+
+  it('should display the first course', () => {
+    component.courses = setupCourses();
+    fixture.detectChanges();
+    const course = component.courses[0];
+    console.log('course', course);
+    console.log('element', el.nativeElement.outerHTML);
+    const card = el.query(By.css('.course-card:first-child')),
+      title = card.query(By.css('mat-card-title')),
+      image = card.query(By.css('img'));
+    expect(card).toBeTruthy('Could not find course card');
+    expect(title.nativeElement.textContent).toBe(course.titles.description);
+    expect(image.nativeElement.src).toBe(course.iconUrl);
   });
 
 });
